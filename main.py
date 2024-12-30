81f050c610ca5a58c8c9088e2f12faff97842037
Rammohan <reddy.kambati@gmail.com>
2024-12-30 11:00:15 +0530

    updated

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import re
 class PersonDetails:
-    def __init__(self,first_name):
+    def __init__(self,first_name,last_name):
         self.first_name=first_name
+        self.last_name=last_name
     def rgex(self):
-        if re.findall(r'^[A-Z][a-zA-Z]{2,}$',self.first_name):
-            return self.first_name
+        if re.match(r'^[A-Z][a-zA-Z]{2,}$',self.first_name):
+            print(f'Your first name is:{self.first_name}')
         else:
-            return "Enter the valid first_name,aggain:"
-    
-obj=PersonDetails("Ram")
-name=obj.rgex()
-print(name)
+            print("Enter the valid first_name,aggain:")
+        
+        if re.match(r'^[A-Z][a-zA-z]{2,}',self.last_name):
+            print(f'your last name is {self.last_name}')
+            print(f'your full name is : {self.first_name + " "+ self.last_name}')
+        else:
+            print("Enter the last name again!!!")
+obj=PersonDetails("Ram","Reddy")
+obj.rgex()
